eb56347ad0fc6441e3469529ccc4cfb5969b4cb2
Pratibha Gauli <166724711+Pr-tibha@users.noreply.github.com>
2025-07-13 21:40:20 +0545

    github commands

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("first")
+echo "# project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Pr-tibha/project.git
+git push -u origin main
+
+
+git remote add origin https://github.com/Pr-tibha/project.git
+git branch -M main
+git push -u origin main
+
+git imp command 
+
+
+aba akdam jilo xa hera hai vaisakyo aba kei garna pardaina asai hunxa gui batai 
